85f2f305050e7ebbb3e1787b9d450b9f25161fc7
matth512 <46283958+matth512@users.noreply.github.com>
2020-07-08 13:49:57 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello Matt</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2> editing to see some magic happen!</h2>
     </div>
   );
 }
