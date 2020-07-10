f6d6528e8b923a397e3556755806d4f029cdbc77
matth512 <46283958+matth512@users.noreply.github.com>
2020-07-10 11:27:24 -0500

    Working Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
 import { Start } from "./Start";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ConfigEditor } from "./ConfigEditor";
 
 export default function App() {
   return (
     <div className="App">
       <h3>Welcome</h3>
-      <Start />
+      <ConfigEditor />
     </div>
   );
 }
---
+++ src/ConfigEditor.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const api =
+  "https://3iwf3m0rwk.execute-api.us-east-2.amazonaws.com/Dev/config/";
+
+export class ConfigEditor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { guid: "34", config: "77" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    // alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+    this.reloadData();
+  }
+
+  reloadData() {
+    console.log("here");
+    fetch(api + this.state.value, {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(response => response.json())
+      //   .then(data => console.log(data));
+      .then(data => this.setState(data));
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <h2>{JSON.stringify(this.state, null, 2)}</h2>
+      </div>
+    );
+  }
+}
