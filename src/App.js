9bff8cf8249ffaf83aa8a46a8ecd1eaa024bab68
matth512 <46283958+matth512@users.noreply.github.com>
2020-07-12 13:20:14 -0500

    Login button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ConfigEditor } from "./ConfigEditor";
 
+const loginUrl =
+  "https://helgren.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id= 38aqmkuo5qpqod397p6nmdij2q&redirect_uri=https%3A%2F%2Fhelgren-lights.s3.us-east-2.amazonaws.com%2F";
+
 export default function App() {
   return (
     <div className="App">
       <h3>Welcome</h3>
+      <button onclick={event => (window.location.href = loginUrl)}>
+        Login with Google
+      </button>
       <ConfigEditor />
     </div>
   );
