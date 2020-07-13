cf0ce8bd065f71fceea1aa072d7c884c243f9c26
matth512 <46283958+matth512@users.noreply.github.com>
2020-07-13 09:41:08 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ConfigEditor } from "./ConfigEditor";
 
 const loginUrl =
-  "https://helgren.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id= 38aqmkuo5qpqod397p6nmdij2q&redirect_uri=https%3A%2F%2Fhelgren-lights.s3.us-east-2.amazonaws.com%2F";
+  "https://helgren.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id= 38aqmkuo5qpqod397p6nmdij2q&redirect_uri=https%3A%2F%2Fhelgren-lights.s3.us-east-2.amazonaws.com%2Findex.html";
 
 export default function App() {
   return (
